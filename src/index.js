c2ca97701c9f5df135fc4b3584ad4dd6a7549964
Saurabh Kumar <34212933+saurabh908@users.noreply.github.com>
2020-08-03 10:58:25 +0530

    Cards first encounter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import "./styles.css";
+let appId = document.getElementById("app");
 
-document.getElementById("app").innerHTML = `
+appId.innerHTML = `
 <h1>Shuffle and Sort</h1>
 `;
+
+const ShuffleButton = () => `<button onClick=shuffleCard()>Shuffle</button>`;
+const SortButton = () => `<button onClick=generateSortedCard()>Sort</button>`;
+
+appId.innerHTML += ShuffleButton();
+appId.innerHTML += SortButton();
+
+const shuffleCard = () => {};
+
+const generateSortedCard = () => {
+  let element = "";
+  for (let index = 0; index < 10; index++) {
+    let elementId = "card" + index;
+    element += `<div class="cardClassDiv">${elementId}</div>`;
+  }
+  appId.innerHTML += element;
+};
+shuffleCard();
+generateSortedCard();
